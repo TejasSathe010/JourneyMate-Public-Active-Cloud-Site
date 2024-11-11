d975a5b47b32141ec0f5a9e8adf8c50bb607cb1c
Tejas Sathe <Tejassathe010@gmail.com>
2024-11-10 23:05:34 -0500

    Push basic code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JourneyMate_Public_Active_Cloud_Site.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
